0f09299349d408eb026d5b36e2141c90345f9e53
Noibisjunior <noibisjunior22@gmail.com>
2025-11-17 10:15:34 +0100

    added the frontend url to the backend to avoid CORS issues

--- BACKEND-NODEJS/app.js
+++ BACKEND-NODEJS/app.js
@@ -7,12 +7,16 @@ const webhookRoutes = require('./routes/webhook');
 
 const app = express();
 
-app.use(
-  cors({
-    origin: 'https://genz-bqagh95by-noibisjuniors-projects.vercel.app',
-    credentials: true
-  })
-);
+app.use(cors({
+  origin: [
+    'https://genz-bqagh95by-noibisjuniors-projects.vercel.app',
+    'https://genz-6cosnszb6-noibisjuniors-projects.vercel.app'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use('/api/webhooks/paystack', webhookRoutes);
 app.use(express.json());
 app.use(cookieParser());
