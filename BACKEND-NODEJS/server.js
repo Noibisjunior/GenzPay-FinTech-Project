050567f9bd6743ccfe5cf349f5832eae0acad26a
Noibisjunior <noibisjunior22@gmail.com>
2025-11-18 09:46:34 +0100

    added vercel.json

--- BACKEND-NODEJS/server.js
+++ BACKEND-NODEJS/server.js
@@ -1,35 +1,57 @@
-//The idea behind this code is to seperate the
-//server code from the express middlewares
-const http = require('http');
-
 const app = require('./app');
-
 const connectDB = require('./Database/connect');
-const router = require('./routes/auth');
 
-const PORT = process.env.PORT || 8009;
+let dbConnected = false;
 
-const server = http.createServer(app);
-app.use('/', router);
+async function ensureDB() {
+  if (!dbConnected) {
+    await connectDB(process.env.MONGO_URI);
+    dbConnected = true;
+  }
+}
 
-const start = async () => {
-    try {
-      await connectDB(process.env.MONGO_URI)
-     //server
-      app.listen(PORT,() => { 
-      console.log(`Server is listening on port ${PORT}`);
-    })
-    } catch (error) {
-      console.log(error);
-    }
-  };
+// Vercel serverless entry function
+module.exports = async (req, res) => {
+  await ensureDB();  
+  return app(req, res);  // hand over request to express app
+};
 
-  start();
-// async function startServer(){
-//   server.listen(PORT, () => {
-//   conso.log(`listening on port ${PORT}...`);
-// });
-// }
 
-// startServer();
+
+
+
+// //The idea behind this code is to seperate the
+// //server code from the express middlewares
+// const http = require('http');
+
+// const app = require('./app');
+
+// const connectDB = require('./Database/connect');
+// const router = require('./routes/auth');
+
+// const PORT = process.env.PORT || 8009;
+
+// const server = http.createServer(app);
+// app.use('/', router);
+
+// const start = async () => {
+//     try {
+//       await connectDB(process.env.MONGO_URI)
+//      //server
+//       app.listen(PORT,() => { 
+//       console.log(`Server is listening on port ${PORT}`);
+//     })
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   start();
+// // async function startServer(){
+// //   server.listen(PORT, () => {
+// //   conso.log(`listening on port ${PORT}...`);
+// // });
+// // }
+
+// // startServer();
 
